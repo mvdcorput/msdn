1cf48afc0e9d0d7892ad524fc06fceb6603fef5f
Martijn van der Corput <mvdcorput@live.nl>
2016-04-17 11:45:41 +0200

    Added text completion extension example project

---
+++ vs2015-extensions/CompletionTest/CompletionTest/TestCompletionSourceProvider.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.Language.Intellisense;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Operations;
+using Microsoft.VisualStudio.Utilities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompletionTest
+{
+    [Export(typeof(ICompletionSourceProvider))]
+    [ContentType("html")]
+    [Name("token completion")]
+    internal class TestCompletionSourceProvider : ICompletionSourceProvider
+    {
+        [Import]
+        internal ITextStructureNavigatorSelectorService NavigatorService { get; set; }
+
+        public ICompletionSource TryCreateCompletionSource(ITextBuffer textBuffer)
+        {
+            return new TestCompletionSource(this, textBuffer);
+        }
+    }
+}
